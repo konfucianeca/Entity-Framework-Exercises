1144eceeff5e1a3952bf6f15d8f5701c80f0bcf8
konfucianeca <vladogg56@gmail.com>
2024-01-07 15:42:20 +0200

    Added WarCroft exam

---
+++ WarCroft/Entities/Characters/Character.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+		private string name;
+        private double health;
+        private double armor;
+        public string Name
+		{
+			get { return name; }
+			private set 
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException(Constants.ExceptionMessages.CharacterNameInvalid);
+
+                }
+				name = value; 
+			}
+		}
+
+        public double BaseHealth { get; set; }
+		public double Health
+		{
+			get { return health; }
+			set 
+			{
+				if (value>BaseHealth)
+				{
+					this.health = BaseHealth;
+				}
+				else if (value < 0)
+				{
+					this.health = 0;
+				}
+				else
+				{
+                    health = value;
+                }
+			}
+		}
+        public double BaseArmor { get; set; }
+
+		public double Armor
+		{
+			get { return armor; }
+			set 
+			{
+				if (value < 0)
+				{
+					this.armor = 0;
+				}
+				else
+				{
+                    armor = value;
+                }
+			}
+		}
+        public double AbilityPoints { get; set; }
+        public Bag Bag { get; set; }
+        public bool IsAlive { get; set; } = true;
+
+		protected void EnsureAlive()
+		{
+			if (!this.IsAlive)
+			{
+				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+			}
+		}
+	}
+}
---
+++ WarCroft/Entities/Inventory/Bag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Inventory
+{
+    public class Bag : IBag
+    {
+        public int Capacity { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public int Load => throw new System.NotImplementedException();
+
+        public IReadOnlyCollection<Item> Items => throw new System.NotImplementedException();
+
+        public void AddItem(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Item GetItem(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
